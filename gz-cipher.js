2796c3a552e383f720753ec60d8c167284f120d3
medifle <13296643+medifle@users.noreply.github.com>
2019-01-06 01:32:27 -0500

    feat($cli): complete encrypt/decrypt logic; use random passwd by default; format the output of keys

--- gz-cipher.js
+++ gz-cipher.js
@@ -9,16 +9,17 @@ const crypto = require('crypto')
 
 const version = '0.0.1'
 let file // filepath
-let passwd = Math.random().toString(36).substring(2)
+let passwd = Math.random()
+  .toString(36)
+  .substring(2)
 let encryptFlag = 2 // 0: encrypt; 1: decrypt; 2: undefined
-
 
 if (process.argv[2] === undefined) {
   throw Error('gz-cipher: No option specified')
 } else if (process.argv[2] === '--help' || process.argv[2] === '-h') {
   console.log('  Usage:')
   console.log('  Encryption:'.padEnd(25) + 'gz-cipher -e file [-p password]')
-  console.log('  Decryption:'.padEnd(25) +  'gz-cipher -d file')
+  console.log('  Decryption:'.padEnd(25) + 'gz-cipher -d file')
   console.log('  -h, --help'.padEnd(25) + 'output usage information')
   console.log('  -v, --version'.padEnd(25) + 'output the version number')
 } else if (process.argv[2] === '--version' || process.argv[2] === '-v') {
@@ -29,7 +30,6 @@ if (process.argv[2] === undefined) {
     if (!options.includes('d')) {
       encryptFlag = 0
     } else {
-      encryptFlag = 2
       throw Error("gz-cipher: Can't specify both -e and -d")
     }
   } else if (options.includes('d')) {
@@ -45,11 +45,13 @@ if (process.argv[2] === undefined) {
     if (fileStats.isFile) {
       file = process.argv[3]
       if (encryptFlag === 0 && process.argv[4]) {
-        if (process.argv[4].substring(0,2) === '-p') {
+        if (process.argv[4].substring(0, 2) === '-p') {
           if (process.argv[5] === undefined) {
             throw Error('gz-cipher: No password specified')
           } else if (process.argv[5].length < 8) {
-            throw Error('gz-cipher: Password must contain at least 8 characters')
+            throw Error(
+              'gz-cipher: Password must contain at least 8 characters'
+            )
           } else {
             passwd = process.argv[5]
           }
@@ -59,69 +61,66 @@ if (process.argv[2] === undefined) {
       }
     }
   }
-
 }
 
+if (encryptFlag !== 2) {
+  const algo = 'aes192'
+  const key = Buffer.concat([Buffer.from(passwd)], 24) // for aes192-cbc, key length is 192bits/8
+  const iv = crypto.randomBytes(16) // for aes, iv length is 128bits/8
 
-console.log("file:",file)
-console.log("passwd",passwd)
+  const reportProgress = new Transform({
+    transform(chunk, encoding, callback) {
+      process.stdout.write('.')
+      callback(null, chunk)
+    }
+  })
 
-// if (!encryptFlag) {
-//   process.exit()
-// }
-
-// const algo = 'aes192'
-// const key = Buffer.concat([Buffer.from(passwd)], 24) // for aes192-cbc, key length is 192bits/8
-// const iv = crypto.randomBytes(16) // for aes, iv length is 128bits/8
-
-// const reportProgress = new Transform({
-//   transform(chunk, encoding, callback) {
-//     process.stdout.write('.')
-//     callback(null, chunk)
-//   }
-// })
-
-// if (encryptFlag) {
-//   /* gzip and encrypt */
-//   fs.createReadStream(file)
-//     .pipe(zlib.createGzip()) // gzip
-//     .pipe(crypto.createCipheriv(algo, key, iv)) // encrypt; key, iv are buffer here
-//     .pipe(reportProgress)
-//     .pipe(fs.createWriteStream(file + '.gz'))
-//     .on('finish', () => console.log('Done'))
-//     .on('close', () => {
-//       fs.writeFile(
-//         file + '_keys.json',
-//         JSON.stringify({
-//           algo,
-//           key: key.toString('hex'),
-//           iv: iv.toString('hex'),
-//           passwd
-//         }),
-//         err => {
-//           if (err) throw err
-//           console.log(file + '_keys.json generated')
-//         }
-//       )
-//       console.log(file + ' has been coded with: ')
-//       console.log('key: ' + key.toString('hex'))
-//       console.log('iv: ' + iv.toString('hex'))
-//     })
-// } else {
-//   /* decrypt and gunzip, use the same ivstr when it was encrypted */
-//   const keys = JSON.parse(
-//     fs.readFileSync(file.slice(0, -3) + '_keys.json', 'utf8')
-//   )
-//   fs.createReadStream(file)
-//     .pipe(
-//       crypto.createDecipheriv(
-//         keys.algo,
-//         Buffer.from(keys.key, 'hex'),
-//         Buffer.from(keys.iv, 'hex')
-//       )
-//     )
-//     .pipe(zlib.createGunzip())
-//     .pipe(reportProgress)
-//     .pipe(fs.createWriteStream('decoded_' + file.slice(0, -3)))
-//     .on('finish', () => console.log('Done'))
-// }
+  if (encryptFlag === 0) {
+    /* gzip and encrypt */
+    fs.createReadStream(file)
+      .pipe(zlib.createGzip()) // gzip
+      .pipe(crypto.createCipheriv(algo, key, iv)) // encrypt; key, iv are buffer here
+      .pipe(reportProgress)
+      .pipe(fs.createWriteStream(file + '.gz'))
+      .on('finish', () => console.log('Done'))
+      .on('close', () => {
+        fs.writeFile(
+          file + '_keys.json',
+          JSON.stringify(
+            {
+              algo,
+              key: key.toString('hex'),
+              iv: iv.toString('hex'),
+              passwd
+            },
+            null,
+            2
+          ),
+          err => {
+            if (err) throw err
+            console.log(file + '_keys.json generated')
+          }
+        )
+        console.log(file + ' has been coded with: ')
+        console.log('key: ' + key.toString('hex'))
+        console.log('iv: ' + iv.toString('hex'))
+      })
+  } else if (encryptFlag === 1) {
+    /* decrypt and gunzip, use the same ivstr when it was encrypted */
+    const keys = JSON.parse(
+      fs.readFileSync(file.slice(0, -3) + '_keys.json', 'utf8')
+    )
+    fs.createReadStream(file)
+      .pipe(
+        crypto.createDecipheriv(
+          keys.algo,
+          Buffer.from(keys.key, 'hex'),
+          Buffer.from(keys.iv, 'hex')
+        )
+      )
+      .pipe(zlib.createGunzip())
+      .pipe(reportProgress)
+      .pipe(fs.createWriteStream('decoded_' + file.slice(0, -3)))
+      .on('finish', () => console.log('Done'))
+  }
+}
